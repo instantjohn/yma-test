36f1e4a5100bd3c06902cf76311ba51677e8d7af
instantjohn <sensiblejohn@hotmail.com>
2021-03-17 20:46:41 +0900

    シンプルにしました。

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ const yma = require('standard-youtube-music-api')
 
 const ttt = new yma.YoutubeMusicAPI();
 
-console.log(ttt.search("hello"));
-console.log(ttt.searchVideos("hello"));
+var res;
 
-// console.log(ttt.headers);
-// console.log(ttt.playerData);
-// console.log(ttt.nextData);
-// console.log(ttt.browseData);
-// console.log(ttt.searchData);
-// console.log(ttt.k);
+ttt.search(ttt.searchData).then(res => res);
